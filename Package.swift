652333954c85d095603727f1ada07fe18534d533
Kuniwak <orga.chem.job@gmail.com>
2017-03-06 23:00:59 +0900

    Move Dwifft source into this package

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "MirrorDiffKit",
-    dependencies: [
-		.Package(url: "https://github.com/Kuniwak/Dwifft.git", majorVersion: 0, minor: 6),
-    ]
+    name: "MirrorDiffKit"
 )
