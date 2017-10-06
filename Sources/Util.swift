6f5a156d73b881d6d7b9825b2be1f7b0a05d2a54
Kuniwak <orga.chem.job@gmail.com>
2017-10-06 10:59:40 +0900

    Strike warnings (#16)
    * Strike Redundunt type warnings
    
    * Strike Unused variables warnings
    
    * Strike Swift4 warnings
    
    * Strike Swift3 @objc inference warnings

--- Sources/Util.swift
+++ Sources/Util.swift
@@ -1,4 +1,4 @@
-func entries<K: Hashable, V>(fromDictionary dictionary: [K: V]) -> [(K, V)] {
+func entries<K, V>(fromDictionary dictionary: [K: V]) -> [(K, V)] {
     var result: [(K, V)] = []
 
     for (key ,value) in dictionary {
--- Tests/MirrorDiffKitTests/ExampleTests.swift
+++ Tests/MirrorDiffKitTests/ExampleTests.swift
@@ -4,31 +4,33 @@ import MirrorDiffKit
 
 class ExampleTests: XCTestCase {
     func testExample1() {
-        let a = Example(
-            key1: "I'm not changed",
-            key2: "I'm deleted"
-        )
-        let b = Example(
-            key1: "I'm not changed",
-            key2: "I'm inserted"
-        )
-
         // NOTE: This is only for the example on README.md
+
+        // let a = Example(
+        //     key1: "I'm not changed",
+        //     key2: "I'm deleted"
+        // )
+        // let b = Example(
+        //     key1: "I'm not changed",
+        //     key2: "I'm inserted"
+        // )
+        //
         // XCTAssertEqual(a, b, diff(between: a, and: b))
     }
 
 
     func testExample2() {
-        let a = NotEquatable(
-            key1: "I'm not changed",
-            key2: "I'm deleted"
-        )
-        let b = NotEquatable(
-            key1: "I'm not changed",
-            key2: "I'm inserted"
-        )
-
         // NOTE: This is only for the example on README.md
+
+        // let a = NotEquatable(
+        //     key1: "I'm not changed",
+        //     key2: "I'm deleted"
+        // )
+        // let b = NotEquatable(
+        //     key1: "I'm not changed",
+        //     key2: "I'm inserted"
+        // )
+        //
         // XCTAssert(
         //     Diffable.from(any: a) =~ Diffable.from(any: b),
         //     diff(between: a, and: b)
